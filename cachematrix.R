e4a6851160ce18da6b608078640b3b6ffc6ecefb
saptarshihere <saptarshihere@gmail.com>
2017-02-25 15:01:30 +0530

    Changes to peer assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+invr <- NULL  
+set <- function(func) {
+x <<- func
+invr <<- NULL 
+}
+get <- function() x 
+setinverse <- function(inverse) invr <<- inverse
+getinverse <- function() invr
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+invr <- x$getinverse()
+if(!is.null(invr)) {
+return(invr)
+}
+data <- x$get()
+invr <- solve(data, ...)
+x$setinverse(invr)
+invr
 }
